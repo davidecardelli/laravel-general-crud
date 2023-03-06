37c5272f53dbcc823d6669e1cdfe3528a60f885b
LeonardoContino <leoconti2000@hotmail.it>
2023-03-06 15:59:23 +0100

    mod css

--- resources/views/includes/card.blade.php
+++ resources/views/includes/card.blade.php
@@ -1,7 +1,7 @@
 <div class="col mb-3">
-    <div class="card d-flex flex-column align-items-center justify-content-center h-100">
+    <div class="card d-flex flex-column align-items-center justify-content-center h-100 p-3">
         <a href="{{ route('cards.show', $card['id']) }}">
-            <img class="p-3 card-img-top" src="{{ $card['figura'] }}" alt="{{ $card['nome_carta'] }}">
+            <img class="card-img-top img-fluid rounded-2" src="{{ $card['figura'] }}" alt="{{ $card['nome_carta'] }}">
         </a>
         <div class="card-body">
             <p class="text-center text-black mt-2 fw-bold text-uppercase">{{ $card['nome_carta'] }}</p>
