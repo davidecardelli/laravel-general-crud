c1392d2ec18ce9f36d9aea0d9ddfc201fa91b0b7
davidecardelli <davide.cardelli.94@gmail.com>
2023-03-06 14:53:59 +0100

    add-create-and-edit

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('content')
+    <main>
+        <div class="container">
+            <h2 class="text-center mb-5">Modifica la card</h2>
+            @include('includes.cards.form')
+        </div>
+    </main>
+@endsection
